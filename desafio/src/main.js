8a173eb10d284393baf0a37d50ed746e8bacf0f3
Anderson Alpin <48166236+AndersonAlpin@users.noreply.github.com>
2021-05-18 14:13:02 -0300

    Tela de login criada

--- desafio/src/main.js
+++ desafio/src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
-import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 
+Vue.config.productionTip = false
 Vue.use(Buefy)
 
-createApp(App).use(store).use(router).mount('#app')
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ desafio/src/views/Login.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="login">
+    <div class="logo">
+      <b-image
+            src="https://pontue.com.br/wp-content/uploads/2018/05/PONTUE-H-2.png"
+        ></b-image>
+    </div>
+
+    <div class="box">
+      <b-field label="Login">
+        <b-input />
+      </b-field>
+
+      <b-field label="Senha">
+        <b-input type="password" password-reveal />
+      </b-field>
+
+      <b-button class="button" expanded>Entrar</b-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+#login {
+  width: 100vw;
+  height: 100vh;
+  background-image: linear-gradient(
+    129deg,
+    rgba(35, 27, 195, 0.7),
+    rgba(152, 19, 177, 0.85)
+  );
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.box {
+  width: 375px;
+  height: 275px;
+  background-color: #fff;
+
+  text-align: left;
+}
+
+.box .button {
+  background-color: #53278c;
+  color: #fff;
+}
+
+.logo {
+  position: absolute;
+  top: 100px;
+  width: 200px;
+}
+</style>
