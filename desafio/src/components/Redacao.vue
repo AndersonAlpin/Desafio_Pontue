ace035b71581fa0c3453e0d1575f12ea73f2b400
Anderson Alpin <48166236+AndersonAlpin@users.noreply.github.com>
2021-05-22 15:17:25 -0300

    Update Redacao.vue

--- desafio/src/components/Redacao.vue
+++ desafio/src/components/Redacao.vue
@@ -1,16 +1,21 @@
 <template>
-  <div v-if="link">
-    <hr>
-    <b-image :src="link"></b-image>
+  <div id="redacao" v-if="link">
+    <hr />
+    <h1 class="subtitle">Redação</h1>
+    <div class="redacao-borda">
+      <b-image :src="link"></b-image>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: ["link"]
-}
+  props: ["link"],
+};
 </script>
 
 <style>
-
+#redacao .redacao-borda {
+  border: 1px solid black;
+}
 </style>
